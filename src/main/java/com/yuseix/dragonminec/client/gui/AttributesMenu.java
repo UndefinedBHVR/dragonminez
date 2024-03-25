bfccb4a28f91845e6f10135952bffab80af2f93c
Bruneitor123 <brunitoaas@gmail.com>
2024-03-25 16:21:21 -0700

    Cambios en el HUD para arreglar #4

--- src/main/java/com/yuseix/dragonminec/client/gui/AttributesMenu.java
+++ src/main/java/com/yuseix/dragonminec/client/gui/AttributesMenu.java
@@ -9,34 +9,32 @@ import com.yuseix.dragonminec.events.ModEvents;
 import com.yuseix.dragonminec.network.C2S.StatsC2S;
 import com.yuseix.dragonminec.network.C2S.ZPointsC2S;
 import com.yuseix.dragonminec.network.ModMessages;
-import com.yuseix.dragonminec.network.S2C.ZPointsS2C;
 import com.yuseix.dragonminec.stats.PlayerStatsAttrProvider;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.FormattedText;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 import org.joml.Quaternionf;
-
 import javax.annotation.Nullable;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AttributesMenu extends Screen {
 
 
-    private int zpoints = Minecraft.getInstance().player.getPersistentData().getInt("zpoints");
+    private int zpoints;
+
+    {
+        assert Minecraft.getInstance().player != null;
+        zpoints = Minecraft.getInstance().player.getPersistentData().getInt("zpoints");
+    }
 
     private String wa = String.valueOf(zpoints);
 
@@ -47,18 +45,21 @@ public class AttributesMenu extends Screen {
 
 
 
-    public AttributesMenu(Component p_96550_) {
-        super(p_96550_);
+    public AttributesMenu(Component pGuiScreen) {
+        super(pGuiScreen);
     }
+
 
 
     @Override
     public void init() {
         super.init();
+
         //MenuInicio
         int posX = (this.width- 152)/2;
         int posY = (this.height- 256)/2;
 
+        assert Minecraft.getInstance().player != null;
         PlayerStatsAttrProvider.getCap(ModEvents.INSTANCE,Minecraft.getInstance().player).ifPresent(playerstats -> {
 
             int zpoints = playerstats.getZpoints();
@@ -110,6 +111,8 @@ public class AttributesMenu extends Screen {
     @Override
     public void tick() {
         super.tick();
+        //No calcular jugadores que no estan en linea pero registrados
+        assert Minecraft.getInstance().player != null;
         PlayerStatsAttrProvider.getCap(ModEvents.INSTANCE,Minecraft.getInstance().player).ifPresent(playerstats -> {
 
             int zpoints = playerstats.getZpoints();
@@ -122,7 +125,7 @@ public class AttributesMenu extends Screen {
 
             if(zpoints >= zCost){
                 botones.forEach(this::addRenderableWidget);
-            } else if (zpoints < zCost){
+            } else {
                 this.clearWidgets();
             }
 
@@ -132,17 +135,16 @@ public class AttributesMenu extends Screen {
     }
 
     @Override
-    public void render(GuiGraphics graphics, int p_281550_, int p_282878_, float p_282465_) {
-        this.renderBackground(graphics);
+    public void render(@NotNull GuiGraphics graphics, int pMouseX, int pMouseY, float pPartialTick) {
+        renderBackground(graphics);
 
-
-
-        int posX = (this.width- 152)/2;
+        int posX = (this.width- 152)/2; //Normalmente da 100... (Ya no se ve)
         int posY = (this.height- 256)/2;
 
         RenderSystem.setShaderColor(1.0f, 1.0f,1.0f,1.0f);
         RenderSystem.setShaderTexture(0,menu);
 
+        assert Minecraft.getInstance().player != null;
         PlayerStatsAttrProvider.getCap(ModEvents.INSTANCE,Minecraft.getInstance().player).ifPresent(playerstats -> {
 
             //PuntosZ
@@ -174,68 +176,80 @@ public class AttributesMenu extends Screen {
             int MaxCon = (int) (con * 0.5)*DMCAttrConfig.MULTIPLIER_CON.get();
             int MaxKiPower = (int) (kipower * 0.5)*DMCAttrConfig.MULTIPLIER_KIPOWER.get();
             int MaxEnergy = (int) (energy * 0.5)*DMCAttrConfig.MULTIPLIER_ENERGY.get();
-            int MaxStamina = (int) (stamina + 3);
+            int MaxStamina = stamina + 3;
 
-//SUBIR STATS
-            graphics.blit(menu,posX-250,posY,0,0,152,256);
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"STATS",posX-190,posY+15,0xFCC3C3, true);
+            int LTITULO = posX-80;
+            int RTITULO = posX +180;
+            int LSUBTITULO = posX-120;
+            int RSUBTITULO = posX+160;
+            int LDESC = posX-60;
+            int RDESC = posX+245;
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"ZPoints: ",posX-230,posY+30,0xFFFFFF, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(zpoints) ,posX-155,posY + 30,0xFFE800, false);
+            //SUBIR STATS
+            //Pos cambiada a -140 de -250 por motivos de cambio de resoluciones para adaptar
+            graphics.blit(menu, posX-140, posY,0,0,152,256);
+            graphics.drawString(font, ChatFormatting.BOLD +"STATS",LTITULO,posY+15,0xFCC3C3, true);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"STR: ",posX-230,posY+45,0x320C0C, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(str) ,posX-170,posY + 45,0xBB1C2A, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"ZPoints: ",LSUBTITULO,posY+30,0xFFFFFF, true);
+            graphics.drawString(font, String.valueOf(zpoints) ,posX-45,posY + 30,0xFFE800, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"DEF: ",posX-230,posY+60,0x320C0C, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(def),posX-170,posY + 60,0xBB1C2A, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"STR: ",LSUBTITULO,posY+45,0x320C0C, true);
+            graphics.drawString(font, String.valueOf(str) ,LDESC,posY + 45,0xBB1C2A, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"CON: ",posX-230,posY+75,0x320C0C, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(con) ,posX-170,posY + 75,0xBB1C2A, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"DEF: ",LSUBTITULO,posY+60,0x320C0C, true);
+            graphics.drawString(font, String.valueOf(def),LDESC,posY + 60,0xBB1C2A, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"POW: ",posX-230,posY+90,0x320C0C, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(kipower) ,posX-170,posY + 90,0xBB1C2A, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"CON: ",LSUBTITULO,posY+75,0x320C0C, true);
+            graphics.drawString(font, String.valueOf(con) ,LDESC,posY + 75,0xBB1C2A, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"ENE: ",posX-230,posY+105,0x320C0C, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(energy) ,posX-170,posY + 105,0xBB1C2A, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"POW: ",LSUBTITULO,posY+90,0x320C0C, true);
+            graphics.drawString(font, String.valueOf(kipower) ,LDESC,posY + 90,0xBB1C2A, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"ZPCost: ",posX-230,posY+120,0xF0B61E, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(zCost),posX-155,posY+120,0xFFE800, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"ENE: ",LSUBTITULO,posY+105,0x320C0C, true);
+            graphics.drawString(font, String.valueOf(energy) ,LDESC,posY + 105,0xBB1C2A, false);
+
+            graphics.drawString(font, ChatFormatting.BOLD +"ZPCost: ",LSUBTITULO,posY+120,0xF0B61E, true);
+            graphics.drawString(font, String.valueOf(zCost),posX-45,posY+120,0xFFE800, false);
 
             //STATS
-            graphics.blit(menu,posX+250,posY,0,0,152,256);
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"INFORMATION",posX+290,posY+15,0xF0B61E, true);
+            graphics.blit(menu,posX+140,posY,0,0,152,256);
+            graphics.drawString(font, ChatFormatting.BOLD +"INFORMATION",RTITULO,posY+15,0xF0B61E, true);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"Damage: ",posX+270,posY+30,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxStr),posX+355,posY+30,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"Damage: ",RSUBTITULO,posY+30,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxStr),RDESC,posY+30,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"Defense: ",posX+270,posY+45,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxDef),posX+355,posY+45,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"Defense: ",RSUBTITULO,posY+45,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxDef),RDESC,posY+45,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"Body: ",posX+270,posY+60,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxCon),posX+355,posY+60,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"Body: ",RSUBTITULO,posY+60,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxCon),RDESC,posY+60,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"Stamina: ",posX+275,posY+75,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxStamina),posX+360,posY+75,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"Stamina: ",RSUBTITULO,posY+75,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxStamina),RDESC,posY+75,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"KiPower: ",posX+270,posY+90,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxKiPower),posX+355,posY+90,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"KiPower: ",RSUBTITULO,posY+90,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxKiPower),RDESC,posY+90,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD +"Max Ki: ",posX+270,posY+105,0x830318, true);
-            graphics.drawString(Minecraft.getInstance().font, String.valueOf(MaxEnergy),posX+355,posY+105,0x9B1D32, false);
+            graphics.drawString(font, ChatFormatting.BOLD +"Max Ki: ",RSUBTITULO,posY+105,0x830318, true);
+            graphics.drawString(font, String.valueOf(MaxEnergy),RDESC,posY+105,0x9B1D32, false);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD + ""+ this.minecraft.player.getName().getString() ,posX+63,posY+10,0xFFFFFF, true);
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD + "Lvl: ",posX+50,posY+25,0xFFE800, true);
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD + "" + String.valueOf(level),posX+80,posY+25,0x67EDFC, true);
+            //Asegurandose de que el jugador se encuentra en linea (no sirve realmente lol)
+            assert this.minecraft != null;
+            assert this.minecraft.player!= null;
+
+            graphics.drawString(font, ChatFormatting.BOLD + this.minecraft.player.getName().getString() ,posX+63,posY+10,0xFFFFFF, true);
+            graphics.drawString(font, ChatFormatting.BOLD + "Lvl: ",posX+50,posY+25,0xFFE800, true);
+            graphics.drawString(font, ChatFormatting.BOLD + String.valueOf(level),posX+80,posY+25,0x67EDFC, true);
 
             renderEntityInInventoryFollowsAngle(graphics, posX+73, posY+200, 80, 0, 0, this.minecraft.player);
 
-            graphics.drawString(Minecraft.getInstance().font, ChatFormatting.BOLD + "Humano :v" ,posX+45,posY+220,0x45E9FC, true);
+            graphics.drawString(font, ChatFormatting.BOLD + "Humano :v" ,posX+45,posY+220,0x45E9FC, true);
 
 
 
         });
 
-        super.render(graphics, p_281550_, p_282878_, p_282465_);
+        super.render(graphics, pMouseX, pMouseY, pPartialTick);
 
     }
 
@@ -266,7 +280,7 @@ public class AttributesMenu extends Screen {
     }
     public static void renderEntityInInventory(GuiGraphics pGuiGraphics, int pX, int pY, int pScale, Quaternionf pPose, @Nullable Quaternionf pCameraOrientation, LivingEntity pEntity) {
         pGuiGraphics.pose().pushPose();
-        pGuiGraphics.pose().translate((double)pX, (double)pY, 50.0);
+        pGuiGraphics.pose().translate(pX, pY, 50.0);
         pGuiGraphics.pose().mulPoseMatrix((new Matrix4f()).scaling((float)pScale, (float)pScale, (float)(-pScale)));
         pGuiGraphics.pose().mulPose(pPose);
         Lighting.setupForEntityInInventory();
@@ -277,9 +291,7 @@ public class AttributesMenu extends Screen {
         }
 
         entityrenderdispatcher.setRenderShadow(false);
-        RenderSystem.runAsFancy(() -> {
-            entityrenderdispatcher.render(pEntity, 0.0, 0.0, 0.0, 0.0F, 1.0F, pGuiGraphics.pose(), pGuiGraphics.bufferSource(), 15728880);
-        });
+        entityrenderdispatcher.render(pEntity, 0.0, 0.0, 0.0, 0.0F, 1.0F, pGuiGraphics.pose(), pGuiGraphics.bufferSource(), 15728880);
         pGuiGraphics.flush();
         entityrenderdispatcher.setRenderShadow(true);
         pGuiGraphics.pose().popPose();
