8933808485d2aee1f33914bf02a1d30f6b679994
Bruneitor123 <32247565+Bruneitor123@users.noreply.github.com>
2024-04-28 15:40:57 -0700

    Create check_lang.sh
    Checkeo de lenguajes por GitHub pa

---
+++ scripts/check_lang.sh
@@ -0,0 +1,17 @@
+FALLIDO=0
+
+for i in ./src/main/resources/assets/dragonminez/lang/*.json
+do
+    if ! jq . "${i}" > /dev/null
+    then
+        echo "El Archivo ${i} es un JSON con formato incorrecto."
+        FALLDIO=1
+    fi
+done
+
+if [ "${FALLIDO}" -ne 0 ]
+then
+    exit 1
+else
+    echo "Archivos de idioma perfectos pa"
+fi
