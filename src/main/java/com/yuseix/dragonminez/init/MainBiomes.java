a5be2539105497dc7aa0e8af99c38456c99b442b
Yuse <dorugamex@gmail.com>
2024-08-26 14:09:05 -0500

    Ya funciona la dimension wa

--- src/main/java/com/yuseix/dragonminez/init/MainBiomes.java
+++ src/main/java/com/yuseix/dragonminez/init/MainBiomes.java
@@ -1,72 +1,13 @@
 package com.yuseix.dragonminez.init;
 
 import com.yuseix.dragonminez.DragonMineZ;
-import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BiomeDefaultFeatures;
-import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.Music;
-import net.minecraft.world.entity.MobCategory;
-import net.minecraft.world.level.biome.*;
-import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraft.world.level.levelgen.SurfaceRules;
-import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
-import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.placement.CaveSurface;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.biome.Biome;
 
 public class MainBiomes {
 
-    private static final Music NORMAL_MUSIC = null;
-
     public static final ResourceKey<Biome> NAMEK_BIOME = ResourceKey.create(Registries.BIOME,
             new ResourceLocation(DragonMineZ.MOD_ID,"namek_biome"));
-
-    public static void bootstrap(BootstapContext<Biome> context){
-        HolderGetter<PlacedFeature> holdergetter = context.lookup(Registries.PLACED_FEATURE);
-        HolderGetter<ConfiguredWorldCarver<?>> holdergetter1 = context.lookup(Registries.CONFIGURED_CARVER);
-
-        context.register(NAMEK_BIOME, namekBiome(holdergetter, holdergetter1));
-
-    }
-
-    public static Biome namekBiome(HolderGetter<PlacedFeature> pPlacedFeatureGetter, HolderGetter<ConfiguredWorldCarver<?>> pConfiguredWorldCarverGetter)
-    {
-        //Esto es para poner mobs que spawneen
-        MobSpawnSettings.Builder spawnBuilder = new MobSpawnSettings.Builder();
-        //Ejemplo de mob spawneando DINO
-        spawnBuilder.addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(MainEntity.DINO1.get(), 8, 4,4));
-
-        BiomeGenerationSettings.Builder biomeBuilder = new BiomeGenerationSettings.Builder(pPlacedFeatureGetter, pConfiguredWorldCarverGetter);
-
-        //Surface Custom, aqui reemplazamos cesped, tierra y piedra
-        SurfaceRules.RuleSource customSurfaceRule = SurfaceRules.sequence(
-                SurfaceRules.ifTrue(SurfaceRules.isBiome(MainBiomes.NAMEK_BIOME),  // Reemplaza con tu bioma registrado
-                        SurfaceRules.sequence(
-                                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(MainBlocks.NAMEK_GRASS_BLOCK.get().defaultBlockState())),
-                                SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, SurfaceRules.state(MainBlocks.NAMEK_DIRT.get().defaultBlockState())),
-                                SurfaceRules.ifTrue(SurfaceRules.abovePreliminarySurface(), SurfaceRules.state(MainBlocks.NAMEK_STONE.get().defaultBlockState()))
-                        )
-                )
-        );
-
-        return biome(Biome.Precipitation.NONE, 2.0F, 0.0F, 0x38A932, 0x38A932, 0x90FF8A, spawnBuilder, biomeBuilder, NORMAL_MUSIC);
-    }
-
-
-
-    private static Biome biome(Biome.Precipitation precipitation, float temperature, float downfall, int waterColor, int waterFogColor, int colorcielo ,int foliageColor,int grassColor, MobSpawnSettings.Builder spawnBuilder, BiomeGenerationSettings.Builder biomeBuilder, Music music)
-    {
-        return (new Biome.BiomeBuilder()).temperature(temperature).downfall(downfall).specialEffects((new BiomeSpecialEffects.Builder()).waterColor(waterColor).waterFogColor(waterFogColor).fogColor(12638463).skyColor(colorcielo).foliageColorOverride(foliageColor).grassColorOverride(grassColor).ambientMoodSound(AmbientMoodSettings.LEGACY_CAVE_SETTINGS).backgroundMusic(music).build()).mobSpawnSettings(spawnBuilder.build()).generationSettings(biomeBuilder.build()).build();
-    }
-
-    private static Biome biome(Biome.Precipitation precipitation, float temperature, float downfall, int waterColor, int waterFogColor, int colorcielo , MobSpawnSettings.Builder spawnBuilder, BiomeGenerationSettings.Builder biomeBuilder, Music music)
-    {
-        return (new Biome.BiomeBuilder()).temperature(temperature).downfall(downfall).specialEffects((new BiomeSpecialEffects.Builder()).waterColor(waterColor).waterFogColor(waterFogColor).fogColor(12638463).skyColor(colorcielo).ambientMoodSound(AmbientMoodSettings.LEGACY_CAVE_SETTINGS).backgroundMusic(music).build()).mobSpawnSettings(spawnBuilder.build()).generationSettings(biomeBuilder.build()).build();
-    }
-
 }
--- src/main/java/com/yuseix/dragonminez/init/MainDimensions.java
+++ src/main/java/com/yuseix/dragonminez/init/MainDimensions.java
@@ -1,72 +1,14 @@
 package com.yuseix.dragonminez.init;
 
-import com.mojang.datafixers.util.Pair;
 import com.yuseix.dragonminez.DragonMineZ;
-import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.*;
-import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
-import net.minecraft.world.level.dimension.DimensionType;
-import net.minecraft.world.level.dimension.LevelStem;
-import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
-
-import java.util.List;
-import java.util.OptionalLong;
 
 public class MainDimensions {
-    public static final ResourceKey<LevelStem> NAMEK_DIM_KEY = ResourceKey.create(Registries.LEVEL_STEM,
-            new ResourceLocation(DragonMineZ.MOD_ID, "namekdim"));
-    public static final ResourceKey<Level> NAMEK_DIM_LEVEL_KEY = ResourceKey.create(Registries.DIMENSION,
-            new ResourceLocation(DragonMineZ.MOD_ID, "namekdim"));
-    public static final ResourceKey<DimensionType> NAMEK_DIM_TYPE = ResourceKey.create(Registries.DIMENSION_TYPE,
-            new ResourceLocation(DragonMineZ.MOD_ID, "namekdim_type"));
-
-
-    public static void bootstrapType(BootstapContext<DimensionType> context) {
-        context.register(NAMEK_DIM_TYPE, new DimensionType(
-                OptionalLong.of(12000), // fixedTime
-                false, // hasSkylight
-                false, // hasCeiling
-                false, // ultraWarm
-                false, // natural
-                1.0, // coordinateScale
-                true, // bedWorks
-                false, // respawnAnchorWorks
-                0, // minY
-                256, // height
-                256, // logicalHeight
-                BlockTags.INFINIBURN_OVERWORLD, // infiniburn
-                BuiltinDimensionTypes.OVERWORLD_EFFECTS, // effectsLocation
-                1.0f, // ambientLight
-                new DimensionType.MonsterSettings(false, false, ConstantInt.of(0), 0)));
-    }
-
-    public static void bootstrapStem(BootstapContext<LevelStem> context) {
-        HolderGetter<Biome> biomeRegistry = context.lookup(Registries.BIOME);
-        HolderGetter<DimensionType> dimTypes = context.lookup(Registries.DIMENSION_TYPE);
-        HolderGetter<NoiseGeneratorSettings> noiseGenSettings = context.lookup(Registries.NOISE_SETTINGS);
-
-        NoiseBasedChunkGenerator wrappedChunkGenerator = new NoiseBasedChunkGenerator(
-                new FixedBiomeSource(biomeRegistry.getOrThrow(MainBiomes.NAMEK_BIOME)),
-                noiseGenSettings.getOrThrow(NoiseGeneratorSettings.AMPLIFIED));
-
-        NoiseBasedChunkGenerator noiseBasedChunkGenerator = new NoiseBasedChunkGenerator(
-                MultiNoiseBiomeSource.createFromList(
-                        new Climate.ParameterList<>(List.of(Pair.of(
-                                        Climate.parameters(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F), biomeRegistry.getOrThrow(MainBiomes.NAMEK_BIOME))
-
-                        ))),
-                noiseGenSettings.getOrThrow(NoiseGeneratorSettings.AMPLIFIED));
-
-        LevelStem stem = new LevelStem(dimTypes.getOrThrow(MainDimensions.NAMEK_DIM_TYPE), noiseBasedChunkGenerator);
-
-        context.register(NAMEK_DIM_KEY, stem);
-    }
+    public static final ResourceKey<Level> NAMEK_DIM = ResourceKey.create(
+            Registries.DIMENSION,
+            new ResourceLocation(DragonMineZ.MOD_ID, "namekdim")
+    );
 }
