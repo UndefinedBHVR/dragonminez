dc58d92cc701fc8ffdcdfb2d837f41df99537968
Shokkoh <ezshokkoh@gmail.com>
2024-11-24 16:59:04 -0300

    - Estructura Kami/Karin

--- src/main/java/com/yuseix/dragonminez/world/StructuresCapability.java
+++ src/main/java/com/yuseix/dragonminez/world/StructuresCapability.java
@@ -6,18 +6,24 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.StructureBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+
+import java.util.Random;
 
 public class StructuresCapability {
 
     private boolean hasTorreKamisama = false;
     private boolean hasHabTiempo = false;
-
-    // Posición donde se generara la torre de kamisama
-    private BlockPos torreKamisamaPosition = null;
-    // Posición hab tiempo
-    private BlockPos habTiempoPos = null;
+    private BlockPos torreKamisamaPosition;
+    private BlockPos torreKarinPosition;
+    private BlockPos habTiempoPos;
 
     public boolean hasTorreKamisama() {
         return hasTorreKamisama;
@@ -35,16 +41,8 @@ public class StructuresCapability {
         this.hasHabTiempo = hasHabTiempo;
     }
 
-    public BlockPos getTorreKamisamaPosition() {
-        return torreKamisamaPosition != null ? torreKamisamaPosition : new BlockPos(100, 45, 100);
-    }
-
     public BlockPos getHabTiempoPos() {
-        return habTiempoPos != null ? habTiempoPos : new BlockPos(-9, 31, -70);
-    }
-
-    public void setTorreKamisamaPosition(BlockPos torreKamisamaPosition) {
-        this.torreKamisamaPosition = torreKamisamaPosition;
+        return habTiempoPos;
     }
 
     public void setHabTiempoPos(BlockPos habTiempoPos) {
@@ -52,14 +50,14 @@ public class StructuresCapability {
     }
 
     public void saveNBTData(CompoundTag nbt) {
-
-        //Torre de Kamisama
         nbt.putBoolean("hasTorreKamisama", hasTorreKamisama);
-        //Hab Tiempo
         nbt.putBoolean("hasHabTiempo", hasHabTiempo);
 
-        if (torreKamisamaPosition != null) {
+        if (torreKamisamaPosition != null || torreKarinPosition != null) {
             nbt.put("torreKamisamaPosition", NbtUtils.writeBlockPos(torreKamisamaPosition));
+            nbt.put("torreKarinPosition", NbtUtils.writeBlockPos(torreKarinPosition));
+            System.out.println("Guardando posición de la Torre de Kami: " + torreKamisamaPosition);
+            System.out.println("Guardando posición de la Torre de Karin: " + torreKarinPosition);
         }
         if (habTiempoPos != null) {
             nbt.put("habTiempoPosition", NbtUtils.writeBlockPos(habTiempoPos));
@@ -67,30 +65,135 @@ public class StructuresCapability {
     }
 
     public void loadNBTData(CompoundTag nbt) {
-
         hasTorreKamisama = nbt.getBoolean("hasTorreKamisama");
         hasHabTiempo = nbt.getBoolean("hasHabTiempo");
 
-        if (nbt.contains("torreKamisamaPosition")) {
+        if (nbt.contains("torreKamisamaPosition") || nbt.contains("torreKarinPosition")) {
             torreKamisamaPosition = NbtUtils.readBlockPos(nbt.getCompound("torreKamisamaPosition"));
+            torreKarinPosition = NbtUtils.readBlockPos(nbt.getCompound("torreKarinPosition"));
+            //System.out.println("Cargando posición de la Torre de Kami: " + torreKamisamaPosition);
+            //System.out.println("Cargando posición de la Torre de Karin: " + torreKarinPosition);
         }
         if (nbt.contains("habTiempoPosition")) {
             habTiempoPos = NbtUtils.readBlockPos(nbt.getCompound("habTiempoPosition"));
         }
     }
 
-    // Generar la estructura si aún no ha sido generada
     public void generateKamisamaStructure(ServerLevel level) {
         if (!hasTorreKamisama) {
-            // Coordenadas específicas para la estructura (100, 45, 100)
-            BlockPos position = new BlockPos(100, 45, 100);
-            spawnTorreKamisama(level, position);
+            Random random = new Random();
+            BlockPos spawnPos = level.getSharedSpawnPos();
 
-            // Marcar como generada y guardar la posición
+            int intentos = 0; // Contador para testear xd
+            BlockPos posicionValida = new BlockPos(0, 0, 0);
+
+            while (posicionValida.equals(new BlockPos(0, 0, 0))) {
+                // Generar posición aleatoria dentro de un rango de 2000 bloques a cada lado desde el spawn
+                int x = spawnPos.getX() + random.nextInt(4000) - 2000;
+                int z = spawnPos.getZ() + random.nextInt(4000) - 2000;
+
+                level.getChunk(x >> 4, z >> 4); // Carga el chunk
+
+                // Obtener altura del terreno en esa posición
+                int y = level.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, x, z);
+
+                // Validar que la altura sea menor a 90 para q no se corte la estructura
+                if (y <= 90) {
+                    BlockPos posiblePos = new BlockPos(x, y, z);
+                    BlockState belowBlockState = level.getBlockState(posiblePos.below());
+                    BlockState belowBelowBlockState = level.getBlockState(posiblePos.below().below());
+
+                    // Validar que la posición sea sólida
+                    if (!belowBlockState.isAir() && !(belowBlockState.getBlock() == Blocks.WATER)
+                            && !belowBelowBlockState.isAir() && !(belowBelowBlockState.getBlock() == Blocks.WATER)) {
+                        posicionValida = posiblePos;
+                        System.out.println("Posición válida encontrada: " + posicionValida);
+                        System.out.println("/teleport Dev " + x + " " + y + " " + z);
+                    }
+                } else {
+                    //System.out.println("No se encontró posición válida, reintentando...");
+                }
+
+                intentos++;
+            }
+
+            if (!posicionValida.equals(new BlockPos(0, 0, 0))) {
+                // Obtener bloques anteriores
+                BlockState blockState = level.getBlockState(posicionValida.below()).getBlock().defaultBlockState();
+                BlockState redstoneBlockState = level.getBlockState(posicionValida.below().offset(1, 0, 0)).getBlock().defaultBlockState();
+                BlockState belowBlockState = level.getBlockState(posicionValida.below().below()).getBlock().defaultBlockState();
+                BlockState belowRedstoneBlockState = level.getBlockState(posicionValida.below().below().offset(1, 0, 0)).getBlock().defaultBlockState();
+
+                // Generar StructureBlocks
+                BlockState structureBlock = Blocks.STRUCTURE_BLOCK.defaultBlockState();
+                BlockState redstoneBlockAdyacente = Blocks.REDSTONE_BLOCK.defaultBlockState();
+
+                // Colocar primer StructureBlock
+                level.setBlock(posicionValida.below(), structureBlock, 3);
+                // Obtener el BlockEntity
+                BlockEntity blockEntity = level.getBlockEntity(posicionValida.below());
+                if (blockEntity instanceof StructureBlockEntity) {
+                    StructureBlockEntity structureBlockEntity = (StructureBlockEntity) blockEntity;
+
+                    // Crear y colocar el NBT
+                    CompoundTag nbtData = new CompoundTag();
+                    nbtData.putString("mirror", "NONE");
+                    nbtData.putString("rotation", "NONE");
+                    nbtData.putInt("posX", -1);
+                    nbtData.putInt("posY", 1);
+                    nbtData.putInt("posZ", -1);
+                    nbtData.putString("mode", "LOAD");
+                    nbtData.putString("name", "dragonminez:kamisama/kami_bottom");
+
+
+                    structureBlockEntity.load(nbtData);
+                    structureBlockEntity.setChanged();
+                    //System.out.println("StructureBlock 1 cargado en " + posicionValida.below() + " con NBT: " + nbtData);
+                    //System.out.println("Comando: /setblock " + posicionValida.below().getX() + " " + posicionValida.below().getY() + " " + posicionValida.below().getZ() + " minecraft:structure_block" + nbtData);
+
+                    // Colocar la Redstone que active el StructureBlock
+                    level.setBlock(posicionValida.below().offset(1, 0, 0), redstoneBlockAdyacente, 3);
+                }
+
+                BlockPos secPos = posicionValida.offset(-3, 0, -3);
+
+                level.setBlock(secPos.below().below(), structureBlock, 3);
+                BlockEntity blockEntityBelow = level.getBlockEntity(secPos.below().below());
+                if (blockEntityBelow instanceof StructureBlockEntity) {
+                    StructureBlockEntity structureBlockEntity = (StructureBlockEntity) blockEntityBelow;
+
+                    CompoundTag nbtData = new CompoundTag();
+                    nbtData.putString("mirror", "NONE");
+                    nbtData.putString("rotation", "NONE");
+                    nbtData.putInt("posX", -51);
+                    nbtData.putInt("posY", 76);
+                    nbtData.putInt("posZ", -51);
+                    nbtData.putString("mode", "LOAD");
+                    nbtData.putString("name", "dragonminez:kamisama/kami_top");
+
+                    structureBlockEntity.load(nbtData);
+                    structureBlockEntity.setChanged();
+                    //System.out.println("StructureBlock 2 cargado en " + secPos.below().below() + " con NBT: " + nbtData);
+                    //System.out.println("Comando: /setblock " + secPos.below().below().getX() + " " + secPos.below().below().getY() + " " + secPos.below().below().getZ() + " minecraft:structure_block" + nbtData);
+
+                    level.setBlock(secPos.below().below().offset(1, 0, 0), redstoneBlockAdyacente, 3);
+                }
+
+                level.setBlock(posicionValida.below(), blockState, 3);
+                level.setBlock(posicionValida.below().offset(1, 0, 0), redstoneBlockState, 3);
+                level.setBlock(secPos.below().below(), belowBlockState, 3);
+                level.setBlock(secPos.below().below().offset(1, 0, 0), belowRedstoneBlockState, 3);
+            }
+
+            BlockPos torreKami = new BlockPos(posicionValida.getX(), posicionValida.getY() + 152, posicionValida.getZ() - 50);
+            BlockPos torreKarin = new BlockPos(posicionValida.getX() - 6, posicionValida.getY() + 56, posicionValida.getZ() - 11);
             setHasTorreKamisama(true);
-            setTorreKamisamaPosition(position);
+            setTorreKamisamaPosition(torreKami);
+            setTorreKarinPosition(torreKarin);
+            System.out.println("Torre Kami: " + getTorreKamisamaPosition()); // Debug para ver si funciona las coord pal comando xd
         }
     }
+
     public void generateHabTiempoStructure(ServerLevel level) {
         if (!hasHabTiempo) {
             BlockPos position = new BlockPos(-9, 31, -70);
@@ -102,15 +205,6 @@ public class StructuresCapability {
         }
     }
 
-    // Método para generar la estructura
-    private void spawnTorreKamisama(ServerLevel level, BlockPos position) {
-        StructureTemplate template = level.getStructureManager().getOrCreate(new ResourceLocation(DragonMineZ.MOD_ID, "kamisama"));
-
-        if (template != null) {
-            StructurePlaceSettings settings = new StructurePlaceSettings();
-            template.placeInWorld(level, position, position, settings, level.getRandom(), 2);
-        }
-    }
     private void spawnHabTiempo(ServerLevel level, BlockPos position) {
         StructureTemplate template = level.getStructureManager().getOrCreate(new ResourceLocation(DragonMineZ.MOD_ID, "habtiempo"));
 
@@ -118,5 +212,17 @@ public class StructuresCapability {
             StructurePlaceSettings settings = new StructurePlaceSettings();
             template.placeInWorld(level, position, position, settings, level.getRandom(), 2);
         }
+    }
+    public void setTorreKamisamaPosition(BlockPos torreKamisamaPosition) {
+        this.torreKamisamaPosition = torreKamisamaPosition;
+    }
+    public void setTorreKarinPosition(BlockPos torreKarinPosition) {
+        this.torreKarinPosition = torreKarinPosition;
+    }
+    public BlockPos getTorreKamisamaPosition() {
+        return torreKamisamaPosition;
+    }
+    public BlockPos getTorreKarinPosition() {
+        return torreKarinPosition;
     }
 }
