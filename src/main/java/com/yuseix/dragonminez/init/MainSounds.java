05dc38fb808b2340d1c44b7153da1f03ae748703
Bruneitor123 <brunitoaas@gmail.com>
2024-11-29 02:11:23 -0800

    sonido (wat?) nave epic

--- src/main/java/com/yuseix/dragonminez/init/MainSounds.java
+++ src/main/java/com/yuseix/dragonminez/init/MainSounds.java
@@ -10,39 +10,41 @@ import net.minecraftforge.registries.RegistryObject;
 
 public final class MainSounds {
 
-    public static final DeferredRegister<SoundEvent> SOUND_EVENTS_REGISTER =
-            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, DragonMineZ.MOD_ID);
+	public static final DeferredRegister<SoundEvent> SOUND_EVENTS_REGISTER =
+			DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, DragonMineZ.MOD_ID);
 
-    public static final RegistryObject<SoundEvent> GOLPE1 = registerSoundEvent("punch1");
-    public static final RegistryObject<SoundEvent> GOLPE2 = registerSoundEvent("punch2");
-    public static final RegistryObject<SoundEvent> GOLPE3 = registerSoundEvent("punch3");
-    public static final RegistryObject<SoundEvent> GOLPE4 = registerSoundEvent("punch4");
-    public static final RegistryObject<SoundEvent> GOLPE5 = registerSoundEvent("punch5");
-    public static final RegistryObject<SoundEvent> GOLPE6 = registerSoundEvent("punch6");
-    public static final RegistryObject<SoundEvent> CRITICO1 = registerSoundEvent("critic_punch1");
-    public static final RegistryObject<SoundEvent> CRITICO2 = registerSoundEvent("critic_punch2");
+	public static final RegistryObject<SoundEvent> GOLPE1 = registerSoundEvent("punch1");
+	public static final RegistryObject<SoundEvent> GOLPE2 = registerSoundEvent("punch2");
+	public static final RegistryObject<SoundEvent> GOLPE3 = registerSoundEvent("punch3");
+	public static final RegistryObject<SoundEvent> GOLPE4 = registerSoundEvent("punch4");
+	public static final RegistryObject<SoundEvent> GOLPE5 = registerSoundEvent("punch5");
+	public static final RegistryObject<SoundEvent> GOLPE6 = registerSoundEvent("punch6");
+	public static final RegistryObject<SoundEvent> CRITICO1 = registerSoundEvent("critic_punch1");
+	public static final RegistryObject<SoundEvent> CRITICO2 = registerSoundEvent("critic_punch2");
 
-    public static final RegistryObject<SoundEvent> RADAR_SCAN = registerSoundEvent("radar_scan");
-    public static final RegistryObject<SoundEvent> DRAGONRADAR = registerSoundEvent("dragonradar");
-    public static final RegistryObject<SoundEvent> NUBE = registerSoundEvent("nube");
-    public static final RegistryObject<SoundEvent> SENZU_BEAN = registerSoundEvent("senzu");
-    public static final RegistryObject<SoundEvent> DRAGONBALLS = registerSoundEvent("dragonballssound");
-    public static final RegistryObject<SoundEvent> SHENRON = registerSoundEvent("shenron");
+	public static final RegistryObject<SoundEvent> RADAR_SCAN = registerSoundEvent("radar_scan");
+	public static final RegistryObject<SoundEvent> DRAGONRADAR = registerSoundEvent("dragonradar");
+	public static final RegistryObject<SoundEvent> NUBE = registerSoundEvent("nube");
+	public static final RegistryObject<SoundEvent> SENZU_BEAN = registerSoundEvent("senzu");
+	public static final RegistryObject<SoundEvent> DRAGONBALLS = registerSoundEvent("dragonballssound");
+	public static final RegistryObject<SoundEvent> SHENRON = registerSoundEvent("shenron");
 
-    public static final RegistryObject<SoundEvent> FROG1 = registerSoundEvent("frogsound1");
-    public static final RegistryObject<SoundEvent> FROG2 = registerSoundEvent("frogsound2");
-    public static final RegistryObject<SoundEvent> FROG3 = registerSoundEvent("frogsound3");
-    public static final RegistryObject<SoundEvent> FROG_LAUGH = registerSoundEvent("froglaugh");
+	public static final RegistryObject<SoundEvent> FROG1 = registerSoundEvent("frogsound1");
+	public static final RegistryObject<SoundEvent> FROG2 = registerSoundEvent("frogsound2");
+	public static final RegistryObject<SoundEvent> FROG3 = registerSoundEvent("frogsound3");
+	public static final RegistryObject<SoundEvent> FROG_LAUGH = registerSoundEvent("froglaugh");
 
-    public static final RegistryObject<SoundEvent> MENU_MUSIC = registerSoundEvent("menu_music");
+	public static final RegistryObject<SoundEvent> MENU_MUSIC = registerSoundEvent("menu_music");
 
-    private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
-        ResourceLocation id = new ResourceLocation(DragonMineZ.MOD_ID, name);
+	public static final RegistryObject<SoundEvent> NAVE_ATERRIZANDO = registerSoundEvent("landing_ship");
 
-        return SOUND_EVENTS_REGISTER.register(name, () -> SoundEvent.createVariableRangeEvent(id));
-    }
+	private static RegistryObject<SoundEvent> registerSoundEvent(String name) {
+		ResourceLocation id = new ResourceLocation(DragonMineZ.MOD_ID, name);
 
-    public static void register(IEventBus busEvent) {
-        SOUND_EVENTS_REGISTER.register(busEvent);
-    }
+		return SOUND_EVENTS_REGISTER.register(name, () -> SoundEvent.createVariableRangeEvent(id));
+	}
+
+	public static void register(IEventBus busEvent) {
+		SOUND_EVENTS_REGISTER.register(busEvent);
+	}
 }
