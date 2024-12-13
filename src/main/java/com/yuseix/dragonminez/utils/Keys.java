04599b85f6291ea45beb07be127265b70c7b40bc
Shokkoh <ezshokkoh@gmail.com>
2024-12-12 21:44:44 -0300

    Turbo Key (Sin Función)

--- src/main/java/com/yuseix/dragonminez/utils/Keys.java
+++ src/main/java/com/yuseix/dragonminez/utils/Keys.java
@@ -9,14 +9,16 @@ public class Keys {
 
     public static final String CATEGORY = "key.categories.dragonminez.main";
 
+    public static final KeyMapping STATS_MENU = new KeyMapping("key.dragonminez.menu_stats",
+            KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, CATEGORY);
     public static final KeyMapping KI_CHARGE = new KeyMapping("key.dragonminez.ki_charge",
             KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_C, CATEGORY);
     public static final KeyMapping DESCEND_KEY = new KeyMapping("key.dragonminez.descend_key",
             KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, CATEGORY);
+    public static final KeyMapping TURBO_KEY = new KeyMapping("key.dragonminez.turbo_key",
+            KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, CATEGORY);
     public static final KeyMapping FUNCTION = new KeyMapping("key.dragonminez.function",
             KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_H, CATEGORY);
-    public static final KeyMapping STATS_MENU = new KeyMapping("key.dragonminez.menu_stats",
-            KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, CATEGORY);
     public static final KeyMapping SELECT_UP = new KeyMapping("key.dragonminez.select_up",
             KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UP, CATEGORY);
     public static final KeyMapping SELECT_DOWN = new KeyMapping("key.dragonminez.select_down",
