ccb97c674c7e29452b933829d9675fb2b3782b3a
Bruneitor123 <brunitoaas@gmail.com>
2024-12-22 12:45:56 -0800

    ricas licencias omg

--- src/main/java/com/yuseix/dragonminez/init/entity/client/model/DendeModel.java
+++ src/main/java/com/yuseix/dragonminez/init/entity/client/model/DendeModel.java
@@ -2,7 +2,6 @@ package com.yuseix.dragonminez.init.entity.client.model;
 
 import com.yuseix.dragonminez.DragonMineZ;
 import com.yuseix.dragonminez.init.entity.custom.DendeEntity;
-import com.yuseix.dragonminez.init.entity.custom.KarinEntity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import software.bernie.geckolib.constant.DataTickets;
@@ -11,30 +10,36 @@ import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.model.data.EntityModelData;
 
-public class DendeModel  extends GeoModel<DendeEntity> {
-    @Override
-    public ResourceLocation getModelResource(DendeEntity dendeEntity) {
-        return new ResourceLocation(DragonMineZ.MOD_ID, "geo/masters/dende.geo.json");
-    }
+/*
+ * This file uses GeckoLib, licensed under the MIT License.
+ * Copyright © 2024 GeckoThePecko.
+ */
 
-    @Override
-    public ResourceLocation getTextureResource(DendeEntity dendeEntity) {
-        return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/masters/dende_master.png");
-    }
+public class DendeModel extends GeoModel<DendeEntity> {
+	@Override
+	public ResourceLocation getModelResource(DendeEntity dendeEntity) {
+		return new ResourceLocation(DragonMineZ.MOD_ID, "geo/masters/dende.geo.json");
+	}
 
-    @Override
-    public ResourceLocation getAnimationResource(DendeEntity dendeEntity) {
-        return new ResourceLocation(DragonMineZ.MOD_ID, "animations/dende.animation.json");
-    }
-    @Override
-    public void setCustomAnimations(DendeEntity animatable, long instanceId, AnimationState<DendeEntity> animationState) {
-        CoreGeoBone head = getAnimationProcessor().getBone("Head");
+	@Override
+	public ResourceLocation getTextureResource(DendeEntity dendeEntity) {
+		return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/masters/dende_master.png");
+	}
 
-        if (head != null) {
-            EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
+	@Override
+	public ResourceLocation getAnimationResource(DendeEntity dendeEntity) {
+		return new ResourceLocation(DragonMineZ.MOD_ID, "animations/dende.animation.json");
+	}
 
-            head.setRotX(entityData.headPitch() * Mth.DEG_TO_RAD);
-            head.setRotY(entityData.netHeadYaw() * Mth.DEG_TO_RAD);
-        }
-    }
+	@Override
+	public void setCustomAnimations(DendeEntity animatable, long instanceId, AnimationState<DendeEntity> animationState) {
+		CoreGeoBone head = getAnimationProcessor().getBone("Head");
+
+		if (head != null) {
+			EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
+
+			head.setRotX(entityData.headPitch() * Mth.DEG_TO_RAD);
+			head.setRotY(entityData.netHeadYaw() * Mth.DEG_TO_RAD);
+		}
+	}
 }
--- src/main/java/com/yuseix/dragonminez/init/entity/client/renderer/DendeRenderer.java
+++ src/main/java/com/yuseix/dragonminez/init/entity/client/renderer/DendeRenderer.java
@@ -3,19 +3,22 @@ package com.yuseix.dragonminez.init.entity.client.renderer;
 import com.yuseix.dragonminez.DragonMineZ;
 import com.yuseix.dragonminez.init.entity.client.model.DendeModel;
 import com.yuseix.dragonminez.init.entity.custom.DendeEntity;
-import com.yuseix.dragonminez.init.entity.custom.KarinEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
-public class DendeRenderer extends GeoEntityRenderer<DendeEntity> {
-    public DendeRenderer(EntityRendererProvider.Context renderManager) {
-        super(renderManager, new DendeModel());
-    }
+/*
+ * This file uses GeckoLib, licensed under the MIT License.
+ * Copyright © 2024 GeckoThePecko.
+ */
 
-    @Override
-    public ResourceLocation getTextureLocation(DendeEntity animatable) {
-        return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/masters/dende_master.png");
-    }
+public class DendeRenderer extends GeoEntityRenderer<DendeEntity> {
+	public DendeRenderer(EntityRendererProvider.Context renderManager) {
+		super(renderManager, new DendeModel());
+	}
+
+	@Override
+	public ResourceLocation getTextureLocation(DendeEntity animatable) {
+		return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/masters/dende_master.png");
+	}
 }
