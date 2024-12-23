b61b88a60223a6f7e4a3cd2dcb6d59275382a4e2
Bruneitor123 <brunitoaas@gmail.com>
2024-12-23 00:17:15 -0800

    Sounds Saiyan ship update Added 5 new sounds:  - ship_landing_open.ogg  - ship_open.ogg  - ui_menu_switch.ogg  - ui_nave_cooldown.ogg  - ui_nave_takeoff.ogg

--- src/main/java/com/yuseix/dragonminez/network/C2S/SpacePodC2S.java
+++ src/main/java/com/yuseix/dragonminez/network/C2S/SpacePodC2S.java
@@ -1,6 +1,7 @@
 package com.yuseix.dragonminez.network.C2S;
 
 import com.yuseix.dragonminez.init.MainEntity;
+import com.yuseix.dragonminez.init.MainSounds;
 import com.yuseix.dragonminez.init.entity.custom.NaveSaiyanEntity;
 import com.yuseix.dragonminez.worldgen.dimension.ModDimensions;
 import net.minecraft.network.FriendlyByteBuf;
@@ -10,60 +11,62 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.network.NetworkEvent;
 
-import java.rmi.registry.Registry;
 import java.util.function.Supplier;
 
 public class SpacePodC2S {
-    private final ResourceKey<Level> targetDimension;
+	private final ResourceKey<Level> targetDimension;
 
-    public SpacePodC2S(ResourceKey<Level> targetDimension) {
-        this.targetDimension = targetDimension;
-    }
+	public SpacePodC2S(ResourceKey<Level> targetDimension) {
+		this.targetDimension = targetDimension;
+	}
 
-    public static void encode(SpacePodC2S msg, FriendlyByteBuf buf) {
-        buf.writeResourceLocation(msg.targetDimension.location());
-    }
+	public static void encode(SpacePodC2S msg, FriendlyByteBuf buf) {
+		buf.writeResourceLocation(msg.targetDimension.location());
+	}
 
-    public static SpacePodC2S decode(FriendlyByteBuf buf) {
-        ResourceLocation location = buf.readResourceLocation();
-        ResourceKey<Level> targetDimension = switch (location.toString()) {
-            case "minecraft:overworld" -> Level.OVERWORLD;
-            case "dragonminez:namek" -> ModDimensions.NAMEK_DIM_LEVEL_KEY;
-            default -> Level.OVERWORLD;
-        };
-        return new SpacePodC2S(targetDimension);
-    }
+	public static SpacePodC2S decode(FriendlyByteBuf buf) {
+		ResourceLocation location = buf.readResourceLocation();
+		ResourceKey<Level> targetDimension = switch (location.toString()) {
+			case "minecraft:overworld" -> Level.OVERWORLD;
+			case "dragonminez:namek" -> ModDimensions.NAMEK_DIM_LEVEL_KEY;
+			default -> Level.OVERWORLD;
+		};
+		return new SpacePodC2S(targetDimension);
+	}
 
-    public static void handle(SpacePodC2S msg, Supplier<NetworkEvent.Context> ctx) {
-        ctx.get().enqueueWork(() -> {
-            if (ctx.get().getSender() != null) {
-                var player = ctx.get().getSender();
-                ServerLevel currentWorld = (ServerLevel) player.level();
-                ServerLevel targetWorld = player.server.getLevel(msg.targetDimension);
-                if (targetWorld != null && player.level() != targetWorld) {
-                    var entities = currentWorld.getEntitiesOfClass(NaveSaiyanEntity.class, player.getBoundingBox().inflate(50.0D));
+	public static void handle(SpacePodC2S msg, Supplier<NetworkEvent.Context> ctx) {
+		ctx.get().enqueueWork(() -> {
+			if (ctx.get().getSender() != null) {
+				var player = ctx.get().getSender();
+				ServerLevel currentWorld = (ServerLevel) player.level();
+				ServerLevel targetWorld = player.server.getLevel(msg.targetDimension);
+				if (targetWorld != null && player.level() != targetWorld) {
+					var entities = currentWorld.getEntitiesOfClass(NaveSaiyanEntity.class, player.getBoundingBox().inflate(50.0D));
 
-                    for (NaveSaiyanEntity nave : entities) {
-                        if (nave.getControllingPassenger() == player) {
-                            nave.discard();
-                            break;
-                        }
-                    }
+					for (NaveSaiyanEntity nave : entities) {
+						if (nave.getControllingPassenger() == player) {
+							nave.discard();
+							break;
+						}
+					}
 
-                    if (player.getY() < 0 ) {
-                        player.teleportTo(targetWorld, player.getX(), player.getY()+120, player.getZ(), player.getYRot(), player.getXRot());
-                    } else if (player.getY() > 0 && player.getY() < 60) {
-                        player.teleportTo(targetWorld, player.getX(), player.getY()+60, player.getZ(), player.getYRot(), player.getXRot());
-                    } else {
-                        player.teleportTo(targetWorld, player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
-                    }
+					if (player.getY() < 0) {
+						player.teleportTo(targetWorld, player.getX(), player.getY() + 120, player.getZ(), player.getYRot(), player.getXRot());
+					} else if (player.getY() > 0 && player.getY() < 60) {
+						player.teleportTo(targetWorld, player.getX(), player.getY() + 60, player.getZ(), player.getYRot(), player.getXRot());
+					} else {
+						player.teleportTo(targetWorld, player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot());
+					}
 
-                    NaveSaiyanEntity naveEntity = new NaveSaiyanEntity(MainEntity.NAVE_SAIYAN.get(), targetWorld);
-                    naveEntity.setPos(player.getX(), player.getY(), player.getZ());
-                    targetWorld.addFreshEntity(naveEntity);
-                    player.startRiding(naveEntity);
-            }
-        }});
-        ctx.get().setPacketHandled(true);
-    }
+					NaveSaiyanEntity naveEntity = new NaveSaiyanEntity(MainEntity.NAVE_SAIYAN.get(), targetWorld);
+					naveEntity.setPos(player.getX(), player.getY(), player.getZ());
+					targetWorld.addFreshEntity(naveEntity);
+					player.startRiding(naveEntity);
+					//Sonido epicardo de la nave abriendose *AL ATERRIZAR*
+					player.playSound(MainSounds.NAVE_LANDING_OPEN.get(), 0.5F, 1.0F);
+				}
+			}
+		});
+		ctx.get().setPacketHandled(true);
+	}
 }
