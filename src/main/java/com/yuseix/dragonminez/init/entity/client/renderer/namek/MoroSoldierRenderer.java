e503d60521f0958c9bde8c1c2ac3894f150fb726
yuseix300 <dorugamex@gmail.com>
2025-01-30 16:15:46 -0500

    Soldiers Render

--- src/main/java/com/yuseix/dragonminez/init/entity/client/renderer/namek/MoroSoldierRenderer.java
+++ src/main/java/com/yuseix/dragonminez/init/entity/client/renderer/namek/MoroSoldierRenderer.java
@@ -1,10 +1,15 @@
 package com.yuseix.dragonminez.init.entity.client.renderer.namek;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.yuseix.dragonminez.DragonMineZ;
 import com.yuseix.dragonminez.init.entity.client.model.namek.MoroSoldierModel;
 import com.yuseix.dragonminez.init.entity.custom.namek.MoroSoldierEntity;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class MoroSoldierRenderer extends GeoEntityRenderer<MoroSoldierEntity> {
@@ -14,5 +19,16 @@ public class MoroSoldierRenderer extends GeoEntityRenderer<MoroSoldierEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(MoroSoldierEntity animatable) {
-        return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/namekusei/soldado1_moro.png");    }
+        return new ResourceLocation(DragonMineZ.MOD_ID, "textures/entity/namekusei/soldado1_moro.png");
+    }
+
+    @Override
+    public RenderType getRenderType(MoroSoldierEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+        return RenderType.entityCutout(texture);
+    }
+
+    @Override
+    protected void renderNameTag(MoroSoldierEntity pEntity, Component pDisplayName, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
+
+    }
 }
